7d3793c9ced3339e3a489a3a41159f4fbcce31db
Angel Rodriguez <angelrodri@hotmail.com>
2022-11-11 14:32:31 -0800

    modified html and js
    added sum-el to reflect the current sum

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ let sum = firstCard + secondCard;
 let hasBlackJack = false;
 let isAlive = true;
 let message = "";
-let messageEl = document.getElementById("message-el")
+let messageEl = document.getElementById("message-el");
+let sumEl = document.getElementById("sum-el");
 
 function startGame() {
     if (sum <= 20) {
@@ -17,4 +18,5 @@ function startGame() {
         isAlive = false;
     }
     messageEl.textContent = message;
+    sumEl.textContent = "Sum: " + sum;
 }
