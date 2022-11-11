985a16e7680f1395c9711c037ec45bb3574da43e
Angel Rodriguez <angelrodri@hotmail.com>
2022-11-11 12:27:41 -0800

    Update index.js
    removed emojis, loaded up message to display

--- index.js
+++ index.js
@@ -4,16 +4,17 @@ let sum = firstCard + secondCard;
 let hasBlackJack = false;
 let isAlive = true;
 let message = "";
+let messageEl = document.getElementById("message-el")
 
 function startGame() {
     if (sum <= 20) {
-        message = "Do you want to draw a new card? 🙂";
+        message = "Do you want to draw a new card?";
     } else if (sum === 21) {
-        message = "Wohoo! You've got Blackjack! 🥳";
+        message = "You've got Blackjack!";
         hasBlackJack = true;
     } else {
-        message = "You're out of the game! 😭";
+        message = "You're out of the game!";
         isAlive = false;
     }
-    console.log(message);
+    messageEl.textContent = message;
 }
