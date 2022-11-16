a90795625fe094ebe8876a4c3042c843593aef1a
Angel Rodriguez <angelrodri@hotmail.com>
2022-11-16 12:47:58 -0800

    modified files
    added NEW CARD button and logged out to console, added padding

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ let isAlive = true;
 let message = "";
 let messageEl = document.getElementById("message-el");
 let sumEl = document.getElementById("sum-el");
+let cardsEl = document.getElementById("cards-el");
 
 function startGame() {
     if (sum <= 20) {
@@ -19,4 +20,9 @@ function startGame() {
     }
     messageEl.textContent = message;
     sumEl.textContent = "Sum: " + sum;
+    cardsEl.textContent = "Cards: " + firstCard+ " " + secondCard;
+}
+
+function newCard() {
+    console.log("Drawing a new card from the deck!");
 }
